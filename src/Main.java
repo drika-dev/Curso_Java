2819c1a2d851a93ed60ea880f4dc22d2ba17c85f
Adriane <leona.vermillion2022@gmail.com>
2023-03-07 14:34:53 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+        System.out.println("Selecione a tarefa: ");
+        System.out.println(" 1 - Positive, Negative or Zero");
+        System.out.println(" 2 - Speed Converter");
+        System.out.println(" 3 - MegaBytes Converter");
+        System.out.println(" 4 - Barking Dog");
+        System.out.println(" 5 - Leap Year Calculator");
+        System.out.println(" 6 - Decimal Comparator");
+        System.out.println(" 7 - Equal Sum Checker");
+        System.out.println(" 8 - Teen Number Checker");
+    }
+}
---
+++ src/SpeedConverter.java
@@ -0,0 +1,19 @@
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            long milesPerHour = Math.round(kilometersPerHour / 1.609);
+            return milesPerHour;
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        if (milesPerHour == -1) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
